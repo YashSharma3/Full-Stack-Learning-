2d51502b01bf2f73f6561d7314974e3925cc3717
yash <sharma2004yash@gmail.com>
2024-12-27 00:45:23 +0530

    all files added

---
+++ JAVASCRIPT/01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) error because we are using node js not browser
+
+/*
+number => 2 to power 53
+bigint use to type big numbers generally used in stock markets
+string => " "
+boolean => true/false
+null => standalone value(representation to the empty value){null is an object}
+undefined => when the variable is not assigned with any value
+symbol => when we need to define the uniqueness
+//object
+*/
+
+console.log(typeof "yash")
